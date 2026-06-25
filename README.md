bbed6f38ac839c736e200812edb2587f7a625e92
dffdff2423 <dffdff2423@gmail.com>
2026-06-25 17:24:53 -0500

    Add dump program

---
+++ README.md
@@ -0,0 +1,13 @@
+# Librobustyaml
+
+Project for dealing with RobustToolbox yaml files. Originally going to be part of my LSP server but was separated out
+into a separate project so other people can use it. Right now still a work on progress.
+
+Unlike other projects that deal with parsing Robust Yaml this directly loads and executes code from the RT assemblies.
+This allows us to be compatible with as many forks as possible and in most contexts that you would want to parse yaml
+you trust the codebase with ACE already or you can run it in a CI box or some other sandboxed environment.
+
+This repo is also home to the yamldocs project which is a WIP website for hosting API documentation of RT
+components, prototypes, etc.
+
+This project follows the REUSE spec for licensing.
---
+++ yamldocs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+using YamlWarrior.Robust;
+using YamlWarrior.Robust.Assemblies;
+
+namespace Yamldocs;
+
+internal static class Program {
+    static void Main(string[] argv) {
+        var flags = new[] {
+            new Flag(Type: ArgumentType.Bool, Long: "dump", HelpText: "Dump info extracted from assemblies as JSON then exit."),
+            new Flag(Type: ArgumentType.String, Long: "assembly-dir", Short: 'a', HelpText: "Path to an ss14 build directory"),
+        };
+        var opts = ArgumentParser.ParseArguments(flags, "-a <assembly> [options...]", argv);
+
+        if (!opts.ContainsKey("assembly-dir")) {
+            Console.Error.WriteLine("Must specify a build directory with -a or --assembly-dir");
+            Environment.Exit(1);
+        }
+
+        var build = (string)opts["assembly-dir"];
+
+        var rtSharedPath = Path.Join(build, AssemblyNames.RobustSharedPath);
+        var yamlCtx = new YamlProcessingContext(rtSharedPath);
+        foreach (var seg in AssemblyNames.DefaultContentAssemblyPathSegments) {
+            yamlCtx.LoadContent(Path.Join(build, seg));
+        }
+
+        if ((bool)opts["dump"]) {
+            Console.WriteLine(JsonSerializer.Serialize(yamlCtx.RobustTypes, new JsonSerializerOptions { WriteIndented = true }));
+            Environment.Exit(0);
+        }
+    }
+}
